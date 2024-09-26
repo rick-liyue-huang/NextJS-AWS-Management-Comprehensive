0992151510f5532db4f5c501871283ef9276f78d
Rick Huang <RickHuang@rauland.com.au>
2024-09-26 12:00:31 +1000

    feat: initially complete the layout

---
+++ src/components/DashboardWrapper/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Navbar } from '@/components/Navbar'
+import { Sidebar } from '@/components/Sidebar'
+
+export const DashboardWrapper = ({
+    children,
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+            <Sidebar />
+            sidebar
+            <main className="flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64">
+                <Navbar />
+                {children}
+            </main>
+        </div>
+    )
+}
